6b4cbabbb45cae67f2b1a9540d0952e8923b2ddb
Josh Finnie <josh.finnie@trackmaven.com>
2016-03-15 17:18:02 -0400

    getting it working locally before dockerzation.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # docker-node-express-skeleton
 A skeleton project that will get you up and running with Node, Express, &amp; ES6 in Docker.
+
+
+## Start
+
+* Run `npm install .` to install the required packages.
+* Run `npm start` to start the web application.
+* Check it out at `http://localhost:8080/api`!
+
+## Todos
+
+* Add test framework and tests
+* Dockerize the application.
---
+++ server/index.js
@@ -0,0 +1,24 @@
+import http from 'http';
+
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+
+import api from './api';
+
+console.log("in server/index.js");
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use('/api', api());
+
+app.server = http.createServer(app);
+app.server.listen(process.env.PORT || 8080);
+
+console.log(`Started on port ${app.server.address().port}.`);
+
+export default app;
